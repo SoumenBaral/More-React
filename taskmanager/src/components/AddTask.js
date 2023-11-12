8911dddf901061b8618cfcaeb9fd1781ee8a3635
SoumenBaral <shumen.sb@gmail.com>
2023-11-12 22:34:21 +0600

    haldle reset

--- taskmanager/src/components/AddTask.js
+++ taskmanager/src/components/AddTask.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddTask = () => {
+    const [taskValue, setTaskValue] = useState("")
+    // const handleChange=(event)=>{
+    //     setTaskValue(event.target.value);
+    // } ****Minified version is given bellow:
+
+    const handleReset = () =>{
+        setTaskValue("");
+
+    }
     return (
        <section className='addTask'>
             <form>
-                <input type="text" name="task" id="task" placeholder='Task Name ' autoComplete='off' />
+                <input onChange={(e)=>setTaskValue(e.target.value)} type="text" name="task" id="task" placeholder='Task Name ' autoComplete='off' value={taskValue} />
                 <button type='submit'>Add Task</button>
-
+                <span onClick={handleReset} className='reset'> Reset </span>
             </form>
+            <p>{taskValue}</p>
        </section>
     );
 };
