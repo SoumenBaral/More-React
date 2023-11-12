76b6f6b040e95ccd5e6f9b83093dc0733f629202
SoumenBaral <shumen.sb@gmail.com>
2023-11-12 21:47:19 +0600

    learn about form

---
+++ taskmanager/src/components/AddTask.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AddTask = () => {
+    return (
+       <section className='addTask'>
+            <form>
+                <label htmlFor="task">Task Name : </label>
+                <input type="text" name="task" id="task" placeholder='Task Name ' autoComplete='off' />
+                <button type='submit'>Add Task</button>
+
+            </form>
+       </section>
+    );
+};
+
+export default AddTask;
