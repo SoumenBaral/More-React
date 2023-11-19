80f1b40dd4687080bfeaa98baac0341b0c3e08b1
SoumenBaral <shumen.sb@gmail.com>
2023-11-19 12:42:21 +0600

    just play with router

--- routemate/src/App.js
+++ routemate/src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+          <Route path='/' element={<Header></Header>}> </Route>
+      </Routes>
     </div>
   );
 }
---
+++ routemate/src/components/ProductDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetails = () => {
+    return (
+        <div>
+            <h1>Please before you enter please Know about me ..</h1>
+        </div>
+    );
+};
+
+export default ProductDetails;
